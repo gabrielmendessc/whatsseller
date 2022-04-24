81dde26e598665802eae00ff92e35bfc1e1ecd5b
gabriel.mendes <gabrielmendes0444@gmail.com>
2022-04-24 16:36:35 -0300

    Initiating implementation

--- src/main/java/com/gabrielmendes/whatsseller/services/BotService.java
+++ src/main/java/com/gabrielmendes/whatsseller/services/BotService.java
@@ -1,6 +1,11 @@
 package com.gabrielmendes.whatsseller.services;
 
+import com.gabrielmendes.whatsseller.WhatsSellerApplication;
 import com.gabrielmendes.whatsseller.entities.IncomingMessage;
+import com.gabrielmendes.whatsseller.entities.UserChat;
+import com.gabrielmendes.whatsseller.resources.UserChatService;
+import com.gabrielmendes.whatsseller.utils.UserChatUtil;
+import com.twilio.Twilio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +14,32 @@ public class BotService {
 
     @Autowired
     private BarcodeService barcodeService;
+    @Autowired
+    private UserChatService userChatService;
 
     public void run(IncomingMessage incomingMessage){
+        Twilio.init(WhatsSellerApplication.ACCOUNT_SID, WhatsSellerApplication.AUTH_TOKEN);
         System.out.println("Bot running ok!");
         System.out.println(incomingMessage.toString());
 
-        if(incomingMessage.getMediaUrl() != null){
-            String barcode = barcodeService.readFromUrl(incomingMessage.getMediaUrl());
-            if(!barcode.equals("")){
-                System.out.println("Barcode: "+barcode);
-            } else {
-                System.out.println("Barcode not found on image");
+        UserChat userChat = userChatService.findByPhoneNumber(incomingMessage.getFrom());
+        if(userChat != null && UserChatUtil.validateMessageInstant(userChat.getMessageInstant())){
+            switch (userChat.getChatChoice()){
+                case MENU:
+
+                case SCANNER:
+
+                case SHOPPING:
+
+                case DELIVERY:
+
+                case PROMOTION:
+
             }
+
+        } else {
+            UserChatUtil.startUserChat(userChat, incomingMessage.getFrom());
+            userChatService.insertUserChat(userChat);
         }
     }
-
 }
