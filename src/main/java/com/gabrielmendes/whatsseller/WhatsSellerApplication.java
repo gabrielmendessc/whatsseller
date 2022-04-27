edde6a0f029753255c971a2b6f96375756a8aeae
gabriel.mendes <gabrielmendes0444@gmail.com>
2022-04-27 18:48:37 -0300

    Removing api-keys

--- src/main/java/com/gabrielmendes/whatsseller/WhatsSellerApplication.java
+++ src/main/java/com/gabrielmendes/whatsseller/WhatsSellerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WhatsSellerApplication {
-    public static final String ACCOUNT_SID = "AC9a3e6957181762ee49aa877c1005599c";
-    public static final String AUTH_TOKEN = "6ed45a3cb2bca5ae4cbb70159706f6b8";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     public static void main(String[] args){
 
